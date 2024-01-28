0c2650dcfe9b5da10c3261a6af70a34b98e262ba
Nate Wolf <nate.wolfrath@gmail.com>
2024-01-28 10:25:33 -0600

    add sample questions

--- questions.py
+++ questions.py
@@ -4,5 +4,21 @@ from modules.MCQ import MCQ
 # MCQ('Question Text', 'Correct Answer')
 
 Questions = [
-    MCQ('','')
+    MCQ("""What is 5^3? 
+        A: 125 
+        B: 625 
+        C: 555
+        D: 5""",'A'),
+
+    MCQ("""What sound does a duck make? 
+        A: Woof woof 
+        B: Moo Moo
+        C: Quack quack
+        D: Oink Oink""",'C'),
+    
+    MCQ("""What is the standard first-line chemotherapy regimen for newly diagnosed advanced-stage epithelial ovarian cancer? 
+        A) Bevacizumab and PARP inhibitors 
+        B) Carboplatin and paclitaxel 
+        C) Doxorubicin and cisplatin 
+        D) Gemcitabine and docetaxel""",'B')
 ]
